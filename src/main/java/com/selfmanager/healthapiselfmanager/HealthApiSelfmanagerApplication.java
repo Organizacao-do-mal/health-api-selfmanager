6a5fee26a58ac7bd4b36737e5a14ff736457b3a4
elias.oliveira <elias.oliveira@adviseu.com.br>
2023-11-22 15:51:34 -0300

    initial commit

---
+++ src/main/java/com/selfmanager/healthapiselfmanager/HealthApiSelfmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.selfmanager.healthapiselfmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthApiSelfmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthApiSelfmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selfmanager/healthapiselfmanager/HealthApiSelfmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfmanager.healthapiselfmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthApiSelfmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
